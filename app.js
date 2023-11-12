eb77aedc2966389512d8ed722cbb14723daef677
bbartika <137605070+bbartika@users.noreply.github.com>
2023-11-12 15:36:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 const express = require('express');
 var cors = require('cors')
 const sequelize = require('./util/database');
-
 const User = require('./models/users');
 const Expense = require('./models/expenses');
-
+const Order = require('./models/orders');
 
 const userRoutes = require('./routes/user')
 const expenseRoutes = require('./routes/expense')
+const purchaseRoutes = require('./routes/purchase')
 
 
 const app = express();
@@ -26,10 +26,14 @@ app.use(express.json());  //this is for handling jsons
 
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
+app.use('/purchase', purchaseRoutes)
+
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync()
     .then(() => {
