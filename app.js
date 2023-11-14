f8a7c4c39685c752be7a4385a240d6987076352f
bbartika <137605070+bbartika@users.noreply.github.com>
2023-11-14 09:45:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const Order = require('./models/orders');
 const userRoutes = require('./routes/user')
 const expenseRoutes = require('./routes/expense')
 const purchaseRoutes = require('./routes/purchase')
-
+const premiumFeatureRoutes = require('./routes/premiumFeature')
 
 const app = express();
 const dotenv = require('dotenv');
@@ -27,7 +27,7 @@ app.use(express.json());  //this is for handling jsons
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
 app.use('/purchase', purchaseRoutes)
-
+app.use('/premium', premiumFeatureRoutes)
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
