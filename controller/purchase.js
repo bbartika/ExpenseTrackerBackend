f0e1aaa62d39d805fbde1ae9d3f05cfa0395f15e
bbartika <137605070+bbartika@users.noreply.github.com>
2023-11-28 09:42:53 +0530

    Update purchase.js

--- controller/purchase.js
+++ controller/purchase.js
@@ -6,17 +6,19 @@ const userController = require('./user')
 const purchasepremium =async (req, res) => {
     try {
         var rzp = new Razorpay({
-            key_id: process.env.RAZORPAY_KEY_ID,
-            key_secret: process.env.RAZORPAY_KEY_SECRET
+            key_id: 'rzp_test_WuARwl5TqUuk5f',//RAZORPAY API KEY ID
+            key_secret: 'SFRC3vmeSYyCjoIfxqnXpeV5'//RAZORPAY SECRET KEY
         })
         const amount = 2500;
 
         rzp.orders.create({amount, currency: "INR"}, (err, order) => {
+        //AT FIRST rezorpay order has been created
             if(err) {
-                throw new Error(JSON.stringify(err));
+                throw new Error(JSON.stringify(err))
             }
+        //THEN requested user's order has been created in the Order Table     
             req.user.createOrder({ orderid: order.id, status: 'PENDING'}).then(() => {
-                return res.status(201).json({ order, key_id : rzp.key_id});
+                return res.status(201).json({ order, key_id :  rzp.key_id});
 
             }).catch(err => {
                 throw new Error(err)
@@ -32,12 +34,14 @@ const purchasepremium =async (req, res) => {
     try {
         const userId = req.user.id;
         const { payment_id, order_id} = req.body;
-        const order  = await Order.findOne({where : {orderid : order_id}}) //2
+        const order  = await Order.findOne({where : {orderid : order_id}}) //2 //IN Order table order id always be unique if same user created multiple orders but each and every time new and unique order id will be generated against the same user.
         const promise1 =  order.update({ paymentid: payment_id, status: 'SUCCESSFUL'}) 
         const promise2 =  req.user.update({ ispremiumuser: true }) 
 
         Promise.all([promise1, promise2]).then(()=> {
-            return res.status(202).json({sucess: true, message: "Transaction Successful", token: userController.generateAccessToken(userId,undefined , true) });
+            return res.status(202).json({sucess: true, message: "Transaction Successful",token:userController.generateAccessToken(userId,undefined, true) });
+                                                                                          //this token again used only for refresh issue of the page,because at the login time token naturally passed, containing user id, ispremiumuser,but in refresh page time how the user's details wlll show again correctly
+                                                                                          //the user's actual details need to pass as token which containg userid,ispremiumuser during update transaction through backend 
         }).catch((error ) => {
             throw new Error(error)
         })
@@ -46,7 +50,7 @@ const purchasepremium =async (req, res) => {
                 
     } catch (err) {
         console.log(err);
-        res.status(403).json({ errpr: err, message: 'Sometghing went wrong' })
+        res.status(403).json({ error : err, message: 'Sometghing went wrong' })
 
     }
 }
